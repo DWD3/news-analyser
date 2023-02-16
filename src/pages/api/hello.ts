cc3a166965f23f9564600acc19cd592a08a483bd
JingCheng Wang <wjcauskew@hotmail.com>
2023-02-16 20:32:39 +1100

    Add mongodb connection

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import clientPromise from '@/services/db/mongodb'
 
 type Data = {
   name: string
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const client = await clientPromise
+  const db = client.db();
+  const collection = await db.collection('news-analyser');
+  const allItems = await collection.find({}).toArray();
+  res.status(200).json({ name: allItems[0].foo })
 }
